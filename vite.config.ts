4498b2b6c24f3bc6eaaa97962e8a21e8c19cf548
North <1227379879@qq.com>
2025-07-08 00:59:56 +0800

    feat: 使用 Vite 和 Element Plus 初始化 Vue 3 项目
    - 为 Element Plus 添加了自定义的暗色和亮色主题。
    - 使用 tsconfig 文件配置了 TypeScript 设置。
    - 使用自动导入和 Element Plus 组件插件设置了 Vite 配置。

---
+++ vite.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import * as path from 'path'
+
+// https://vite.dev/config/
+export default defineConfig({
+    resolve: {
+        alias: {
+            '@/': path.resolve(__dirname, './src/')
+        }
+    },
+    plugins: [
+        vue(),
+        AutoImport({
+            resolvers: [ElementPlusResolver()]
+        }),
+        Components({
+            resolvers: [ElementPlusResolver()]
+        })
+    ],
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: `@use "~/styles/element/index.scss" as *;`
+            }
+        }
+    }
+})
