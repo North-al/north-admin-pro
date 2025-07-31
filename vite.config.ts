018661dd87d5db33eec55fcc42f52a7e44a2d67d
North <1227379879@qq.com>
2025-07-31 22:01:25 +0800

    feat: 添加 element-plus 组件优化依赖，更新 Vite 配置以支持 CSS 代码拆分

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import * as path from 'path'
+import { elementPlusOptimizeDeps } from './build/element-plus'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -50,5 +51,42 @@ export default defineConfig({
                 // additionalData: `@use "~/styles/el-dark.scss" as *;`
             }
         }
+    },
+    build: {
+        cssCodeSplit: true,
+        // 预加载一些组件、模块等
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        if (id.includes('vue')) {
+                            return 'vue'
+                        }
+                        if (id.includes('element-plus')) {
+                            return 'element-plus'
+                        }
+                        if (id.includes('pinia')) {
+                            return 'pinia'
+                        }
+                        return 'vendor'
+                    }
+
+                    // 将 src/components 下的组件打包到 components 中
+                    if (id.includes('src/components')) {
+                        return 'components'
+                    }
+
+                    if (id.includes('src/hooks') || id.includes('src/utils')) {
+                        return 'hooks-utils'
+                    }
+                },
+                chunkFileNames: 'assets/js/[name]-[hash].js',
+                entryFileNames: 'assets/js/[name]-[hash].js',
+                assetFileNames: 'assets/[ext]/[name]-[hash].[ext]'
+            }
+        }
+    },
+    optimizeDeps: {
+        include: ['vue', 'vue-router', 'pinia', 'element-plus', 'element-plus/es', ...elementPlusOptimizeDeps()]
     }
 })
